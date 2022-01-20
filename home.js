1138d8d991bac1748e8ea077c6b17d3c5ee63337
auzunder <edgarnogueiratavares@gmail.com>
2022-01-20 14:42:34 +0000

    pop ups

--- home.js
+++ home.js
@@ -1,23 +1,52 @@
-// Get the modal
-var modal = document.getElementById("materiaisPopUp");
 
-// Get the button that opens the modal
-var btn = document.getElementById("medidaMateriais");
 
-// Get the <span> element that closes the modal
+var btn_01 = document.getElementById("medidaMateriais");
+var btn_02 = document.getElementById("medidaESEC");
+var btn_03 = document.getElementById("medidaCaloiros");
+var btn_04 = document.getElementById("medidaMateriais");
+var btn_05 = document.getElementById("medidaMateriais");
+var btn_06 = document.getElementById("medidaMateriais");
+var btn_07 = document.getElementById("medidaMateriais");
+var btn_08 = document.getElementById("medidaMateriais");
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn_01.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
+}
+btn_02.onclick = function(){
+    modal = document.getElementById("ESECPopUp");
+    modal.style.display = "flex";
+}
+btn_03.onclick = function(){
+    modal = document.getElementById("CaloirosPopUp");
+    modal.style.display = "flex";
+}
+btn_04.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
+}
+btn_05.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
+}
+btn_06.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
+}
+btn_07.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
+}
+btn_08.onclick = function(){
+    modal = document.getElementById("materiaisPopUp");
+    modal.style.display = "flex";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
