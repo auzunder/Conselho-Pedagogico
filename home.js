e7f506711d84744d35a90940e0cf61f9dde8ecd0
auzunder <edgarnogueiratavares@gmail.com>
2022-01-23 02:10:44 +0000

    Correção de erros

--- home.js
+++ home.js
@@ -8,47 +8,67 @@ var btn_05 = document.getElementById("medidaCartao");
 var btn_06 = document.getElementById("medidaAlunos");
 var btn_07 = document.getElementById("medidaNovoPolo");
 var btn_08 = document.getElementById("medidaErasmus");
-var span = document.getElementsByClassName("close")[0];
+
 
 btn_01.onclick = function(){
     modal = document.getElementById("materiaisPopUp");
+    document.getElementById("close_1").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_02.onclick = function(){
     modal = document.getElementById("ESECPopUp");
+    document.getElementById("close_2").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_03.onclick = function(){
     modal = document.getElementById("CaloirosPopUp");
+    document.getElementById("close_3").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_04.onclick = function(){
     modal = document.getElementById("cursosPopUp");
+    document.getElementById("close_4").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_05.onclick = function(){
     modal = document.getElementById("cartaoPopUp");
+    document.getElementById("close_5").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_06.onclick = function(){
     modal = document.getElementById("alunosPopUp");
+    document.getElementById("close_6").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_07.onclick = function(){
     modal = document.getElementById("NovoPoloPopUp");
+    document.getElementById("close_7").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 btn_08.onclick = function(){
     modal = document.getElementById("ErasmusPopUp");
+    document.getElementById("close_8").onclick = function() {
+        modal.style.display = "none";
+    };
     modal.style.display = "flex";
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
