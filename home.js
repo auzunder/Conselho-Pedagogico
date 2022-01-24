797c22919d828e94adb754d909fc42d816b8a71c
auzunder <edgarnogueiratavares@gmail.com>
2022-01-24 01:55:52 +0000

    Animations

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ var btn_02 = document.getElementById("medidaESEC");
 var btn_03 = document.getElementById("medidaCaloiros");
 var btn_04 = document.getElementById("medidaCursos");
 var btn_05 = document.getElementById("medidaCartao");
-var btn_06 = document.getElementById("medidaAlunos");
+var btn_06 = document.getElementById("medidaHorários");
 var btn_07 = document.getElementById("medidaNovoPolo");
 var btn_08 = document.getElementById("medidaErasmus");
 
@@ -46,7 +46,7 @@ btn_05.onclick = function(){
     modal.style.display = "flex";
 }
 btn_06.onclick = function(){
-    modal = document.getElementById("alunosPopUp");
+    modal = document.getElementById("horáriosPopUp");
     document.getElementById("close_6").onclick = function() {
         modal.style.display = "none";
     };
